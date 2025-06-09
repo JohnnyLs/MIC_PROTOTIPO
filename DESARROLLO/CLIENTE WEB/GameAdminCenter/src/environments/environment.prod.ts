87059a09ec6ab9ef9359641258cb3b13ec573c94
Johnny Loachamin <Jsloachamin1@espe.edu.ec>
2025-06-08 23:36:52 -0500

    Add Creditos screen

--- DESARROLLO/CLIENTE WEB/GameAdminCenter/src/environments/environment.prod.ts
+++ DESARROLLO/CLIENTE WEB/GameAdminCenter/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.tujuego.com/api/' // Cambia esto por la URL de producción
+  apiBaseUrl: 'https://https://game-backend.onrender.com/api/'
 };
